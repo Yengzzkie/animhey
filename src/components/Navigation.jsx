e31163b5077e16e615e00f4d7807e37f2d6538a4
Yengzzkie <gatchalian.manuel@gmail.com>
2024-06-20 21:25:24 -0400

    increased navbar z index

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -56,7 +56,7 @@ export default function Navigation() {
   );
 
   return (
-    <div className="sticky top-0 z-10">
+    <div className="sticky top-0 z-50">
       <Navbar className="bg-[#161616] border-none sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-white-900">
           <Typography
